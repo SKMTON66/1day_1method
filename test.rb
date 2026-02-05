07e10174223837235f29ae2bfdd071aac0a784e2
TONGARARIN <tongararin@TONGARARINnoMacBook-Air.local>
2026-02-05 22:41:46 +0900

    Revert "ファイル名を間違えた"
    This reverts commit 3c08482ee7fb8fcdfc522e1130247e8a977a4109.

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 ary = [1, 2]
 ary_2 = [3, 4]
 
-p ary.push(3) #=> [1, 2, 3]
-p ary.push(ary_2) #=> [1, 2, 3, [3, 4]]
-p ary_2.push #=> [3, 4]
+p ary.product(ary_2)
+
+ary.product(ary_2) { print it }
