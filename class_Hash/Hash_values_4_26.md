84f31e7e9e20ca15ea7fd5f587a2b2ca15a1b975
TONGARARIN <tongararin@gmail.com>
2026-04-26 10:22:24 +0900

    4/26

---
+++ class_Hash/Hash_values_4_26.md
@@ -0,0 +1,10 @@
+- [Hash\#values \(Ruby 4\.0 リファレンスマニュアル\)](https://docs.ruby-lang.org/ja/latest/method/Hash/i/values.html) <br>
+values -> [object]
+
+ハッシュの全値を配列にして返すメソッド。
+
+```rb
+hash = { a: "Alice", b: "Bob", c: "Charlie" }
+
+p hash.values #=> ["Alice", "Bob", "Charlie"]
+```
--- class_Hash/foo.rb
+++ class_Hash/foo.rb
@@ -1,7 +1,3 @@
-hash1 = { a: "Alice", b: "Bob", c: "Charlie" }
-hash2 = { b: "Bond", c: "Chris" }
+hash = { a: "Alice", b: "Bob", c: "Charlie" }
 
-p hash1.update(hash2) #=> {a: "Alice", b: "Bond", c: "Chris"}
-
-p hash1.update(hash2) { |key, hash1_value, hash2_value| "#{hash1_value} or #{hash2_value}"}
-#=> {a: "Alice", b: "Bond or Bond", c: "Chris or Chris"}
+p hash.values #=> ["Alice", "Bob", "Charlie"]
